35fd4f10dbb66033fdfb2986ba627ef1a8b3951e
Joan Blanco <joanblanco.dev@gmail.com>
2024-02-12 09:45:39 -0400

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,51 @@
+import {useReducer, useEffect} from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => JSON.parse(localStorage.getItem("todos")) || [];
+export const useTodos = (initialState = []) => {
+
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+      }, [todos]);
+
+    // Function to add a new TODO
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+
+  };
+
+  // Function to remove only one TODO
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  }
+
+  // Function to check and uncheck a TODO
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  return{
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todosCount : todos.length,
+    pendingTodosCount : todos.filter(todo => !todo.done).length,
+  }
+}
+
